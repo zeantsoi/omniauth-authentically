7c42f9d42b18de1eb8bc741e6bb615e279b716a6
zeantsoi <zeantsoi@gmail.com>
2012-02-01 21:28:37 -0700

    Update lib/omniauth/strategies/authentically.rb

--- lib/omniauth/strategies/authentically.rb
+++ lib/omniauth/strategies/authentically.rb
@@ -6,7 +6,7 @@ module OmniAuth
 
       option :name, 'authentically'
       option :client_options, {
-      	:site => 'http://localhost:3000',
+      	:site => 'http://authentically.heroku.com',
       	:authorize_path => '/oauth/authorize'#,
       	#:token_path => '/oauth/access_token'
       }
